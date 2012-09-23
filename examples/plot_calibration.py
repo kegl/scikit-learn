fbb8355ca67c3121efbdcea80772e2050fbb7ef2
Alexandre Gramfort <alexandre.gramfort@inria.fr>
2012-09-23 17:57:32 +0200

    ENH : new IsotonicCalibrator

--- examples/plot_calibration.py
+++ examples/plot_calibration.py
@@ -1,7 +1,15 @@
 """
-================
-Calibration plot
-================
+============================
+Probability Calibration plot
+============================
+
+When performing classfication you often want to predict, not only
+the class label, but also the associated probability. This probability
+gives you some kind of confidence on the prediction. This example
+demonstrates the use of various estimators that can predict probabilities
+as well as some estimators that can be used to calibrate the
+output score into a probability. The accuracy is estimated
+with Brier's score.
 
 """
 print __doc__
@@ -18,34 +26,18 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import brier_score, calibration_plot
 from sklearn.linear_model import IsotonicRegression
 from sklearn.svm import SVC
+from sklearn.calibration import IsotonicCalibrator
 
-# X, y = make_classification(n_samples=5000, random_state=42)
-
-rng = np.random.RandomState(42)
-n_samples = 1000
-std = 1.0
-X1 = std * rng.randn(n_samples, 2)
-X2 = np.r_[np.array([2, 2]) + std * rng.randn(n_samples / 2, 2),
-           np.array([5, 5]) + std * rng.randn(n_samples / 2, 2)]
-X = np.concatenate([X1, X2], axis=0)
-y = np.r_[np.zeros(n_samples), np.ones(n_samples)].ravel()
-
-order = np.arange(len(X))
-rng.shuffle(order)
-X = X[order]
-y = y[order]
-del order
-
-# Add some outliers and reshuffle
-outliers_idx = rng.randint(n_samples * 2, size=100)
-y[outliers_idx] = 1 - y[outliers_idx]
+n_samples = 3000
+n_bins = 8  # for calibration_plot
+X, y = make_classification(n_samples=2 * n_samples, n_features=6,
+                           random_state=42)
 
 # split train, test and OOB for calibration
 X_train, y_train = X[:n_samples], y[:n_samples]
 X_train_oob, y_train_oob = X[:n_samples / 2], y[:n_samples / 2]
 X_oob, y_oob = X[n_samples / 2:], y[n_samples / 2:]
 X_test, y_test = X[n_samples:], y[n_samples:]
-n_bins = 8  # for calibration_plot
 
 # Logistic Regression
 clf = LogisticRegression(C=1., intercept_scaling=100.)
@@ -57,36 +49,10 @@ clf = SVC(C=1., probability=True)
 clf.fit(X_train, y_train)
 prob_pos_svc = clf.predict_proba(X_test)[:, 1]
 
-# Ref:
-# Obtaining calibrated probability estimates from decision trees
-# and naive Bayesian classifiers, ICML 2001
-
 # Logistic Regression with isotonic calibration
-clf = LogisticRegression(C=1., intercept_scaling=100.)
-clf.fit(X_train_oob, y_train_oob)
-
-# compute binning
-bins = np.linspace(0, 1.0, n_bins)
-binids = np.digitize(clf.predict_proba(X_oob)[:, 1], bins) - 1
-bin_proba = list()
-for k in xrange(n_bins - 1):
-    y_k = y_oob[binids == k]
-    if y_k.size:
-        bin_proba.append(np.mean(y_k))
-
-bin_proba = np.array(bin_proba)
-
-ir = IsotonicRegression()
-prob_oob = np.sort(clf.predict_proba(X_oob)[:, 1])
-prob_calibrated_oob = bin_proba[np.searchsorted(bins, prob_oob) - 1]
-
-ir.fit(prob_oob, prob_calibrated_oob)
-prob_pos_lr_ir = ir.predict(clf.predict_proba(X_test)[:, 1])
-
-# SGD Log
-clf = SGDClassifier(loss="log", seed=0, penalty='l2', n_iter=200)
-clf.fit(X_train, y_train)
-prob_pos_sgd_log = clf.predict_proba(X_test)[:, 1]
+ir_clf = IsotonicCalibrator(LogisticRegression(C=1., intercept_scaling=100.))
+ir_clf.fit(X_train_oob, y_train_oob, X_oob, y_oob)
+prob_pos_lr_ir = ir_clf.predict_proba(X_test)[:, 1]
 
 # SGD Huber
 clf = SGDClassifier(loss="modified_huber", seed=0, penalty='l2', n_iter=200)
@@ -103,27 +69,19 @@ pt_lr, pp_lr = calibration_plot(y_test, prob_pos_svc, bins=n_bins)
 print "LogisticRegression (IR): %1.3f" % brier_score(y_test, prob_pos_lr_ir)
 pt_lr_ir, pp_lr_ir = calibration_plot(y_test, prob_pos_lr_ir, bins=n_bins)
 
-print "SGD (log): %1.3f" % brier_score(y_test, prob_pos_sgd_log)
-pt_sgd, pp_sgd = calibration_plot(y_test, prob_pos_sgd_log, bins=n_bins)
-
 print "SGD (modified_huber): %1.3f" % brier_score(y_test, prob_pos_sgd_huber)
-pt_sgd2, pp_sgd2 = calibration_plot(y_test, prob_pos_sgd_huber, bins=n_bins)
+pt_sgd, pp_sgd = calibration_plot(y_test, prob_pos_sgd_huber, bins=n_bins)
 
 ###############################################################################
 # Plot calibration plots
 
 import pylab as pl
-
 pl.close('all')
 
 pl.figure()
-pl.scatter(X_test[y_test == 0, 0], X_test[y_test == 0, 1], color='r')
-pl.scatter(X_test[y_test == 1, 0], X_test[y_test == 1, 1], color='g')
-
-pl.figure()
 order = np.lexsort((y_test, prob_pos_lr))
-pl.plot(prob_pos_lr[order], 'xr', label='prob(y_)')
-pl.plot(prob_pos_lr_ir[order], 'b', linewidth=3, label='prob(y_) (IR)')
+pl.plot(prob_pos_lr[order], 'xr', label='Logistic prob(y_)')
+pl.plot(prob_pos_lr_ir[order], 'b', linewidth=3, label='Isotonic Logistic prob(y_)')
 pl.plot(y_test[order], 'sg', linewidth=3, label='y')
 pl.ylim([-0.05, 1.05])
 pl.legend(loc="center left")
@@ -132,12 +90,11 @@ pl.show()
 pl.figure()
 pl.xlabel("Predicted probability")
 pl.ylabel("True probability")
-pl.plot([0, 1], [0, 1], "b", label="Perfectly calibrated")
-pl.plot(pp_lr, pt_lr, "ms-", label="LogisticRegression")
-pl.plot(pp_lr, pt_lr, "cs-", label="SVC + Platt")
-pl.plot(pp_lr_ir, pt_lr_ir, "ks-", label="LogisticRegression (IR)")
-pl.plot(pp_sgd, pt_sgd, "rs-", label="SGDClassifier (log)")
-pl.plot(pp_sgd2, pt_sgd2, "gs-", label="SGDClassifier (huber)")
+pl.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
+pl.plot(pp_lr, pt_lr, "rs-", label="LogisticRegression")
+pl.plot(pp_lr, pt_lr, "gs--", label="SVC + Platt")
+pl.plot(pp_lr_ir, pt_lr_ir, "bs-", label="LogisticRegression (IR)")
+pl.plot(pp_sgd, pt_sgd, "cs-", label="SGDClassifier (huber)")
 pl.legend(loc="lower right")
 pl.ylim([-0.05, 1.05])
 pl.show()
---
+++ sklearn/calibration.py
@@ -0,0 +1,93 @@
+"""Calibration estimators."""
+
+# Author: Alexandre Gramfort <alexandre.gramfort@inria.fr>
+# License: BSD Style
+
+import numpy as np
+
+from .base import BaseEstimator
+from .linear_model import IsotonicRegression
+
+
+class IsotonicCalibrator(BaseEstimator):
+    """Probability calibration with Isotonic Regression
+
+    Parameters
+    ----------
+    estimator : BaseEstimator
+        The estimator whose output decision function needs to be calibrated.
+
+    Notes
+    -----
+    Reference:
+    Obtaining calibrated probability estimates from decision trees
+    and naive Bayesian classifiers, B. Zadrozny & C. Elkan, ICML 2001
+    """
+    def __init__(self, estimator):
+        self.estimator = estimator
+
+    def fit(self, X, y, X_oob, y_oob):
+        """Fit the calibrated model
+
+        Parameters
+        ----------
+        X : array-like, shape = [n_samples, n_features]
+            Training data.
+
+        y : array-like, shape = [n_samples]
+            Target values.
+
+        X_oob : array-like, shape = [n_samples, n_features]
+            Out of bag training data used for calibration.
+
+        y_oob : array-like, shape = [n_samples]
+            Out of bag targets used for calibration.
+
+        Returns
+        -------
+        self : object
+            returns an instance of self.
+        """
+        self.estimator.fit(X, y)
+        df = self.estimator.decision_function(X_oob)
+        if df.shape[1] > 1:
+            raise ValueError('IsotonicCalibrator only support binary classification.')
+        df = df.ravel()
+        order = np.argsort(df)
+        df = df[order]
+        self._ir = IsotonicRegression(x_min=0., x_max=1.)
+        self._ir.fit(df, y_oob[order])
+        return self
+
+    def predict_proba(self, X):
+        """
+        This function return posterior probabilities of classification
+        according to each class on an array of test vectors X.
+
+        Parameters
+        ----------
+        X : array-like, shape = [n_samples, n_features]
+
+        Returns
+        -------
+        C : array, shape = [n_samples, 2]
+            The predicted probas.
+        """
+        proba = self._ir.predict(self.estimator.decision_function(X).ravel())
+        proba = np.c_[1. - proba, proba]
+        return proba
+
+    def predict(self, X):
+        """Predit the target of new samples.
+
+        Parameters
+        ----------
+        X : array-like, shape = [n_samples, n_features]
+
+        Returns
+        -------
+        C : array, shape = [n_samples]
+            The predicted class.
+        """
+        prob = self.predict_proba(X)[:, 1]
+        return prob > 0.5
